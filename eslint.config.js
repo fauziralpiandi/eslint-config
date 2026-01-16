6e331e032ca1d9178a2e4fb718ea83c6478e009c
Fauzira Alpiandi <fauziralpiandi.dev@gmail.com>
2026-01-16 21:11:09 +0000

    docs: update

--- eslint.config.js
+++ eslint.config.js
@@ -1,32 +1,6 @@
-import eslint from './dist/index.js';
+import config from '@fauziralpiandi/eslint-config';
 
-export default eslint(
-  {
-    typescript: true,
-    jsonc: true,
-    ignores: [
-      'dist/**',
-      'node_modules/**',
-      'tsup.config.ts',
-      '.cache/**',
-      '.upm/**',
-    ],
-  },
-  {
-    files: ['src/**/*.ts'],
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-unsafe-assignment': 'off',
-      '@typescript-eslint/no-unsafe-member-access': 'off',
-    },
-  },
-  {
-    files: ['test/**/*.ts'],
-    rules: {
-      '@typescript-eslint/no-unsafe-assignment': 'off',
-      '@typescript-eslint/no-unsafe-call': 'off',
-      '@typescript-eslint/no-unsafe-member-access': 'off',
-      '@typescript-eslint/no-unsafe-return': 'off',
-    },
-  },
-);
+export default config({
+  typescript: true,
+  ignores: ['**/*.test.ts'],
+});
